1f6e4c0c1960147ef1a4607823e4053da12cf780
Tania <taniachanana02@gmail.com+Tania@users.noreply.github.com>
2022-03-04 16:47:51 +0530

    Weather app commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Temp from './whether/temp'
+import React from 'react'
+
+const App = () => {
+  return (
+    <Temp />
+    //<Todo />
+    // <UseReducer/>
+    // <UseEffect />
+    // <UseState />
+    // <Restaurant />
+  )
+}
+
+export default App;
